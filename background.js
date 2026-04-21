24a54edca49d613f69421ec5f041728bd271596c
Evan Genske <genskevan@gmail.com>
2026-04-21 01:26:50 -0400

    it works!

---
+++ background.js
@@ -0,0 +1,15 @@
+const search_results = 'https://www.google.com/search*';
+
+chrome.action.onClicked.addListener(async (tab) => {
+    if (tab.url.startsWith(search_results)) {
+        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+        // Next state will always be the opposite
+        const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+        // Set the action badge to the next state
+        await chrome.action.setBadgeText({
+            nextState,
+        });
+    }
+});
--- scripts/content.js
+++ scripts/content.js
@@ -1,48 +1,10 @@
-function renderReadingTime(article) {
-  // If we weren't provided an article, we don't need to render anything.
-  if (!article) {
-    console.log("no article");
-    return;
-  }
+function removeOverview() {
 
-  console.log("should be working now...");
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+    const style = document.createElement("style");
+    style.textContent = ".bzXtMb.M8OgIe.dRpWwb { display: none; }"; // these are the classes assigned to the whole overview segment
+    // since we're firing this script off before loading the DOM, we can't add a hidden attribute.
+    // this preemptively targets the div and defaults it to hidden
+    document.documentElement.appendChild(style);
 }
 
-renderReadingTime(document.querySelector("article"));
-
-const observer = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    // If a new article was added.
-    for (const node of mutation.addedNodes) {
-      if (node instanceof Element && node.tagName === 'ARTICLE') {
-        // Render the reading time for this particular article.
-        renderReadingTime(node);
-      }
-    }
-  }
-});
-
-// https://developer.chrome.com/ is a SPA (Single Page Application) so can
-// update the address bar and render new content without reloading. Our content
-// script won't be reinjected when this happens, so we need to watch for
-// changes to the content.
-observer.observe(document.querySelector('devsite-content'), {
-  childList: true
-});
+removeOverview();
